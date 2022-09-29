937227d281d35589e88df3e0ecfd40fc7160a4d4
Anand Stephan <anandstephan@Anands-MacBook-Pro.local>
2022-09-29 02:50:46 -0700

    initial commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Anand Stephan on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
